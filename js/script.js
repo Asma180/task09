a2dfb7aed8222da6fa196f59d2490466f3729da3
asma180 <asmaulhusna1527@gmail.com>
2019-12-22 13:53:51 +0600

    all file added

---
+++ js/script.js
@@ -0,0 +1,12 @@
+
+$(document) .ready(function(){
+    $(".owl-carousel") .owlCarousel({
+        items:3,
+        autoplay: true,
+        autoplayTimeout: 2000,
+        autoplayHoverPause: true,
+        loop: true,
+        margin: 50,
+		dots: true,
+    });
+});
